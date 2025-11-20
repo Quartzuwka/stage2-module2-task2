1ce10cd281778acd69c48f229ed5e248e2241410
kvashnin <kvashnin.semion@yandex.ru>
2025-11-20 10:38:38 +0300

    maybe?

--- src/main/java/com/example/listener/ContextListener.java
+++ src/main/java/com/example/listener/ContextListener.java
@@ -1,5 +1,21 @@
 package com.example.listener;
 
-public class ContextListener {
-    //write your code here!
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.time.LocalDateTime;
+
+@WebListener
+public class ContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        LocalDateTime initTime = LocalDateTime.now();
+
+        sce.getServletContext().setAttribute("servletTimeInit", initTime);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
 }
--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -1,5 +1,44 @@
 package com.example.servlet;
 
-public class LoginServlet {
-    //write your code here!
+import com.example.Users;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Object user = req.getSession().getAttribute("user");
+
+        if (user!=null) {
+            req.getRequestDispatcher("/user/hello.jsp").forward(req, resp);
+        } else {
+            req.getRequestDispatcher("/login.jsp").forward(req, resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+        List<String> allowedUsers = Users.getInstance().getUsers();
+
+        boolean isUserExist = allowedUsers.contains(login);
+        boolean isPasswordValid = password != null;
+
+        if (isUserExist && isPasswordValid) {
+            req.getSession().setAttribute("user", login);
+            req.getRequestDispatcher("/user/hello.jsp").forward(req, resp);
+        } else {
+            req.getRequestDispatcher("/login.jsp").forward(req, resp);
+        }
+    }
 }
